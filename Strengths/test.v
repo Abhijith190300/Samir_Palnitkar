a8534a155e0412b3023a4aba168179ec43d32d42
Abhijith190300 <86602036+Abhijith190300@users.noreply.github.com>
2021-06-29 10:08:15 +0530

    Displaying the resultant strengths

--- Strengths/test.v
+++ Strengths/test.v
@@ -10,4 +10,11 @@ module test;
     #250    in0=1'b0;in1=1'b1;
     #250    in0=1'b1;in1=1'b1;
     end
+  initial
+   begin
+   #125    $display("At time %t, \n \t in0 has value %b and strength %v,\n \t in1 has value %b and strength %v,\n \t out has value %b and strength %v \n\n",$time,in0,in0,in1,in1,out,out);
+   #250    $display("At time %t, \n \t in0 has value %b and strength %v,\n \t in1 has value %b and strength %v,\n \t out has value %b and strength %v \n\n",$time,in0,in0,in1,in1,out,out);
+   #250    $display("At time %t, \n \t in0 has value %b and strength %v,\n \t in1 has value %b and strength %v,\n \t out has value %b and strength %v \n\n",$time,in0,in0,in1,in1,out,out);
+   #250    $display("At time %t, \n \t in0 has value %b and strength %v,\n \t in1 has value %b and strength %v,\n \t out has value %b and strength %v \n\n",$time,in0,in0,in1,in1,out,out);
+   end
 endmodule
